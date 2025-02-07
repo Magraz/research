d70df464a7acc12958e79f90f20169e886f13850
Magraz <agrazvallejo@live.com>
2025-02-07 12:07:39 -0800

    renamed import names

--- src/learning/algorithms/manual/control.py
+++ src/learning/algorithms/manual/control.py
@@ -4,8 +4,8 @@ import torch
 
 from learning.environments.types import EnvironmentParams
 from learning.environments.create_env import create_env
-from learning.algorithms.ippo.PPO import Params
-from learning.algorithms.ippo.IPPO import IPPO
+from learning.algorithms.ippo.ppo import Params
+from learning.algorithms.ippo.ippo import IPPO
 import numpy as np
 import pickle as pkl
 from pathlib import Path
