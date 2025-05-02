048a81c8212d04bb09aeb4c1ee8189f574fe8651
Magraz <agrazvallejo@live.com>
2025-05-02 15:40:25 -0700

    more checkpoint loading fixes

--- src/learning/algorithms/ppo/train.py
+++ src/learning/algorithms/ppo/train.py
@@ -136,17 +136,17 @@ class PPO_Trainer:
         )
 
         # Checkpoint loading logic
+        data = []
         if self.checkpoint:
+
             checkpoint_path = self.models_dir / "checkpoint"
+
             if checkpoint_path.is_file():
                 # Load checkpoint
                 learner.load(checkpoint_path)
                 # Load data
                 with open(self.models_dir / "data.dat", "rb") as f:
                     data = pkl.load(data)
-        else:
-            # Start with empty data
-            data = []
 
         # Setup loop variables
         step = 0
